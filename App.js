c04de3a331636b241679c588d4c4b0ae7eeb3828
Sitirohmah29 <srsitirohmah054@gmail.com>
2024-07-22 16:20:34 +0700

    fikry yang upload

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { StatusBar, Text, View } from 'react-native';
+import SignIn from './Components/SignIn/SignIn';
+import Splash from './Components/Splash/Splash';
+import User from './Components/User/User';
+
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {{ flex:1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Splash" component={Splash} options={{ headerShown:false }}/>
+          <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown:false }}/>
+          <Stack.Screen name="User" component={User} options={{ headerShown:false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
